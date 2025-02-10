407ed97c0769705f29945f157fa25052d6a73891
rubaiiyat <abirr1516@gmail.com>
2025-02-10 22:44:02 +0600

    complete project

--- 05-SImple-Project/index.js
+++ 05-SImple-Project/index.js
@@ -2,8 +2,25 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.write("Working");
-  res.end();
+  if (req.url === "/") {
+    fs.readFile("home.html", (error, data) => {
+      res.end(data);
+    });
+  } else if (req.url === "/list") {
+    fs.readFile("productDetails.html", (error, data) => {
+      res.end(data);
+    });
+  } else if (req.url === "/about") {
+    fs.readFile("about.html", (error, data) => {
+      res.end(data);
+    });
+  } else if (req.url === "/contact") {
+    fs.readFile("contact.html", (error, data) => {
+      res.end(data);
+    });
+  } else {
+    res.end("Page Not Found");
+  }
 });
 
 server.listen(5000, () => {
