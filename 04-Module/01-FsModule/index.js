9c15c546c6b5dec3ecf1a176a516b6f943a05c0a
rubaiiyat <abirr1516@gmail.com>
2025-02-10 13:24:42 +0600

    work with fs sync

--- 04-Module/01-FsModule/index.js
+++ 04-Module/01-FsModule/index.js
@@ -2,11 +2,16 @@ const http = require("http");
 const fs = require("fs");
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    fs.readFile("index.html", function (error, data) {
+    /* fs.readFile("index.html", function (error, data) {
       res.writeHead(200, { "content-type": "text/html" });
       res.write(data);
       res.end();
-    });
+    }); */
+
+    const mydata = fs.readFileSync("index.html");
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(mydata);
+    res.end();
   }
 });
 
