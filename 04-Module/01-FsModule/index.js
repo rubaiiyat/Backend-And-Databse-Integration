754270ba0fcc647f6691e33447154429280d3685
rubaiiyat <abirr1516@gmail.com>
2025-02-10 13:15:30 +0600

    work with fs file

---
+++ 04-Module/01-FsModule/index.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile("index.html", function (error, data) {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
