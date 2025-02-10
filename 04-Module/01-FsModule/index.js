3de7810e6cab6d71f7179790f9baa1d909225290
rubaiiyat <abirr1516@gmail.com>
2025-02-10 16:22:47 +0600

    work with write file

--- 04-Module/01-FsModule/index.js
+++ 04-Module/01-FsModule/index.js
@@ -1,17 +1,34 @@
 const http = require("http");
 const fs = require("fs");
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    /* fs.readFile("index.html", function (error, data) {
+  // work with read file
+  /* if (req.url === "/") {
+    fs.readFile("index.html", function (error, data) {
       res.writeHead(200, { "content-type": "text/html" });
       res.write(data);
       res.end();
-    }); */
+    });
 
     const mydata = fs.readFileSync("index.html");
     res.writeHead(200, { "content-type": "text/html" });
     res.write(mydata);
     res.end();
+  } */
+
+  // work with write file
+
+  if (req.url === "/") {
+    fs.writeFile("test.txt", "I will be win", function (error) {
+      if (error) {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.write("Failed");
+        res.end();
+      } else {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.write("This is home page");
+        res.end();
+      }
+    });
   }
 });
 
